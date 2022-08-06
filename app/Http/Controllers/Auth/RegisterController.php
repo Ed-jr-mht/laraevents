d9951ebad26f560e1c7ec199abd5895191294035
EDSON <cg1502841@LAB117M26.IFSP>
2022-08-05 22:10:28 -0300

    Laravel Parte feita até 44:58 min da segunda parte

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,9 +3,27 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+    public function create() {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        $requestData= $request->all();
+        $requestData['role'] = 'participant';
+
+        //User::create($requestData);
+
+       // $password = bcrypt($requestData['password']);
+
+       //$requestData['password'] = $password;
+
+       User::create($requestData);
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('register',[RegisterController::class, 'create'])->name('auth.register.create');
+Route::post('register', [RegisterController::class, 'store'])->name('auth.register.store');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('teste',function(){
+    return \App\Models\User::all();
 });
