fbd4f53f7580422cac9c25a0eb4a28a201a8deeb
Ed-jr-mht <107441157+Ed-jr-mht@users.noreply.github.com>
2022-08-17 21:02:33 -0300

    o Request arrumado para validar Telefone

--- public/js/auth/register.js
+++ public/js/auth/register.js
@@ -1,7 +1,7 @@
 $('.cpf').mask('000.000.000-00');
     $('.cep').mask('00000-000');
     $('.uf').mask('SS');
-    $('.phone').mask('(00) 00000-0000');
+    $('.phone').mask('(00) 0000-0000');
     $('.cellphone').mask('(00) 00000-0000');
 
     $(document).on('blur','#cep', function(){
